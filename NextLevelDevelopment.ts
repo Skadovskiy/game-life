6931f7bc352a137d9886982fc3f045bbaf625e11
Skadovskiy <Skadovskiy@ubrr.ru>
2023-09-19 17:17:47 +0500

    learning stream api

--- NextLevelDevelopment.ts
+++ NextLevelDevelopment.ts
@@ -1,9 +1,9 @@
 const initState: boolean[][] = [
     [false, false, false, false, false, false, false, false, false, false],
     [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false, false],
-    [false, false, true, true, true, false, false, false, false, false],
+    [false, false, false, true, true, false, false, false, false, false],
+    [false, false, false, true, false, false, false, false, false, false],
+    [false, false, true, false, false, false, false, false, false, false],
     [false, false, false, false, false, false, false, false, false, false],
     [false, false, false, false, false, false, false, false, false, false],
     [false, false, false, false, false, false, false, false, false, false],
@@ -21,15 +21,12 @@ function NextLevelDevelopment(currentState: boolean[][]): boolean[][] {
                 row.map(
                     (isLivingCell, x) => {
                         const width: number = row.length - 1;
-                        const toxicityLevel: number = region.reduce(
-                            (sum, r) =>
-                                sum + ((
-                                    (y + r.y) >= 0 &&
-                                    (x + r.x) >= 0 &&
-                                    (y + r.y) <= height &&
-                                    (x + r.x) <= width &&
-                                    currentState[y + r.y][x + r.x]) ? 1 : 0)
-                            , 0);
+                        const toxicityLevel: number = region.map(
+                            (r) => ({ x: (x + r.x), y: (y + r.y) }))
+                            .filter(
+                                (f) => f.y >= 0 && f.x >= 0 && f.y <= height && f.x <= width)
+                            .reduce(
+                                (sum, r) => sum + ((currentState[r.y][r.x]) ? 1 : 0), 0);
 
                         const isSurvivor: boolean = isLivingCell && (toxicityLevel == 2 || toxicityLevel == 3);
                         const isBirthCell: boolean = !isLivingCell && toxicityLevel == 3;
